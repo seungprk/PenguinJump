e32faa04d7f08445b87f113a5af828d43fa72472
mtso <matthewtso@gmail.com>
2016-05-23 03:49:16 -0700

    Create Iceberg Generator
    Created iceberg polygon generation.
    Prototyped iceberg shadow functionality.
    SKShapeNode creation needs to be fixed and Iceberg generator needs to
    be refactored into an SKSpriteNode subclass for integration into the
    game.

---
+++ IcebergGenerator/IcebergGenerator/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  IcebergGenerator
+//
+//  Created by Matthew Tso on 5/22/16.
+//  Copyright (c) 2016 De Anza. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene(size: view.bounds.size)
+        let skView = view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.showsPhysics = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .AspectFit// .ResizeFill
+        skView.presentScene(scene)
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
